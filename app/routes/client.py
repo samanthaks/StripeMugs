834c60f0a37343d5fd320f3309323e6f76562313
Raymond Xu <raymondxumail@gmail.com>
2017-02-03 19:10:42 -0500

    Implement storeItem listing

--- app/routes/client.py
+++ app/routes/client.py
@@ -1,11 +1,16 @@
 from flask import session, redirect, url_for, render_template, request, jsonify, Response, flash, Blueprint
-# from ..forms import CreateForm
-# from ..models import Player
+import pprint
+import requests
+import json
 
 
 client = Blueprint('client', __name__)
+DB_BASE_URL = 'https://lotgul9df8.execute-api.us-west-2.amazonaws.com/prod/'
 
 
 @client.route('/', methods=['GET'])
 def index():
-	return render_template('index.html')
+	r = requests.get(DB_BASE_URL + '/storeItems')
+	store_items = json.loads(r.json())['Items']
+	print(store_items)
+	return render_template('index.html', storeItems=store_items)
